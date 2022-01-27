92532eb8a76fc0a7a6e16c3d2c8eee1bdcd03a54
shmg1679 <91004979+shmg1679@users.noreply.github.com>
2022-01-26 22:11:50 -0500

    added all frontend project from ttp

---
+++ frontend_projects/The_choice/script.js
@@ -0,0 +1,11 @@
+//.left the . is for the space in the class name in html
+//ex: <div class="split left"> split is for css, the left will be js like here .left
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
