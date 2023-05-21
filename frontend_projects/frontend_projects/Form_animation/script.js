b154bf5f881f3390e9ae25fbc6f1da4a73b2e899
shmg1679 <shimingh1479@gmail.com>
2023-05-20 22:47:27 -0400

    .

--- frontend_projects/frontend_projects/Form_animation/script.js
+++
@@ -1,9 +0,0 @@
-const labels = document.querySelector('.form-control label')
-
-labels.foreach(label=>{
-    label.innerHTML = label.innerText
-    .split('')
-    .map((letter,idx)=>`<span style="transition-delay:${idx
-    *50}">${letter}</span>`)
-    .join('')
-});
--- frontend_projects/frontend_projects/change_bg_color/myscript.js
+++
@@ -1,8 +0,0 @@
-var colors = ['red', 'blue', 'green', 'maroon', 'Crimson', 'Teal', 'Sienna']
-
-i = 0;
-
-$("change-color").click(function(){
-    $("div").css("backgroundColor", colors[i]);
-    i = (i==colors.length-1) ? 0 : (i+1);
-});
